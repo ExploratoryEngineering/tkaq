bec5b7bca1bf8cbeee00bc535fbe7461333e8bdb
Per Kristian Kummermo <per.kristian@telenordigital.com>
2019-03-25 15:08:55 +0100

    refactor(dataController): add DataQuery data class

--- src/main/kotlin/tkaq/data/DataController.kt
+++ src/main/kotlin/tkaq/data/DataController.kt
@@ -4,44 +4,51 @@ import io.javalin.Context
 import tkaq.DB
 import java.time.Instant
 
+data class DataQuery(
+        val fromTimestamp: Long,
+        val toTimestamp: Long,
+        val limit: Int
+)
+
+fun Context.dataQuery() = DataQuery(
+        fromTimestamp = this.validatedQueryParam("since", "0").asLong().getOrThrow(),
+        toTimestamp = this.validatedQueryParam("until", Instant.now().toEpochMilli().toString()).asLong().getOrThrow(),
+        limit = this.validatedQueryParam("limit", "255").asInt().getOrThrow()
+)
+
+
 object DataController {
     fun getData(ctx: Context) {
-        val fromTimestamp = ctx.validatedQueryParam("since", "0").asLong().getOrThrow()
-        val toTimeStamp = ctx.validatedQueryParam("until", Instant.now().toEpochMilli().toString()).asLong().getOrThrow()
-        val limit = ctx.validatedQueryParam("limit", "255").asInt().getOrThrow()
+        val dataQuery = ctx.dataQuery()
 
         ctx.json(DB.retrieveDataPoints(
-                fromTimestamp = fromTimestamp,
-                toTimestamp = toTimeStamp,
-                limit = limit
+                fromTimestamp = dataQuery.fromTimestamp,
+                toTimestamp = dataQuery.toTimestamp,
+                limit = dataQuery.limit
         ))
     }
 
     fun getDataForCollection(ctx: Context) {
-        val collectionId = ctx.validatedPathParam("collection-id").getOrThrow()
-        val fromTimestamp = ctx.validatedQueryParam("since", "0").asLong().getOrThrow()
-        val toTimeStamp = ctx.validatedQueryParam("until", Instant.now().toEpochMilli().toString()).asLong().getOrThrow()
-        val limit = ctx.validatedQueryParam("limit", "255").asInt().getOrThrow()
+        val dataQuery = ctx.dataQuery()
+        val collectionId = ctx.pathParam("collection-id")
 
         ctx.json(DB.retrieveDataPointsForCollection(
-                fromTimestamp = fromTimestamp,
-                toTimestamp = toTimeStamp,
-                limit = limit,
+                fromTimestamp = dataQuery.fromTimestamp,
+                toTimestamp = dataQuery.toTimestamp,
+                limit = dataQuery.limit,
                 collectionId = collectionId
         ))
     }
 
     fun getDataForCollectionDevice(ctx: Context) {
-        val collectionId = ctx.validatedPathParam("collection-id").getOrThrow()
-        val deviceId = ctx.validatedPathParam("device-id").getOrThrow()
-        val fromTimestamp = ctx.validatedQueryParam("since", "0").asLong().getOrThrow()
-        val toTimeStamp = ctx.validatedQueryParam("until", Instant.now().toEpochMilli().toString()).asLong().getOrThrow()
-        val limit = ctx.validatedQueryParam("limit", "255").asInt().getOrThrow()
+        val dataQuery = ctx.dataQuery()
+        val collectionId = ctx.pathParam("collection-id")
+        val deviceId = ctx.pathParam("device-id")
 
         ctx.json(DB.retrieveDataPointsForCollectionDevice(
-                fromTimestamp = fromTimestamp,
-                toTimestamp = toTimeStamp,
-                limit = limit,
+                fromTimestamp = dataQuery.fromTimestamp,
+                toTimestamp = dataQuery.toTimestamp,
+                limit = dataQuery.limit,
                 collectionId = collectionId,
                 deviceId = deviceId
         ))
